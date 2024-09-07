eb11a8e7bd8dc6fffa42d0e66c122be2b6ae5f50
Huy Ngo <huybngo@yahoo.com>
2024-09-07 08:48:06 -0700

    Add sync funcs

--- tests/Blazor.Core.Tests/Components/AfterRenderExecuterTests.cs
+++ tests/Blazor.Core.Tests/Components/AfterRenderExecuterTests.cs
@@ -3,7 +3,7 @@ namespace Blazor.Core.Tests.Components;
 public class AfterRenderExecuterTests
 {
   [Fact]
-  public async Task ExecuteFuncsAsync_FuncReturnsTrue_NoRetry()
+  public async Task ExecuteFuncsAsync_AsyncFuncReturnsTrue_NoRetry()
   {
     // Arrange
     var executer = new AfterRenderExecuter();
@@ -18,7 +18,7 @@ public class AfterRenderExecuterTests
   }
 
   [Fact]
-  public async Task ExecuteFuncsAsync_FuncReturnsFalse_RetryIsExecuted()
+  public async Task ExecuteFuncsAsync_AsyncFuncReturnsFalse_RetryIsExecuted()
   {
     // Arrange
     var successResult = false;
@@ -43,4 +43,47 @@ public class AfterRenderExecuterTests
     Assert.Equal(1, retryExecutedCount);
     Assert.Equal(0, retryFailedCount);
   }
+
+  [Fact]
+  public void ExecuteFuncs_FuncReturnsTrue_NoRetry()
+  {
+    // Arrange
+    var executer = new AfterRenderExecuter();
+    executer.Add(() => true);
+
+    // Act
+    var (executedCount, failedCount) = executer.ExecuteFuncs();
+
+    // Assert
+    Assert.Equal(1, executedCount);
+    Assert.Equal(0, failedCount);
+  }
+
+  [Fact]
+  public void ExecuteFuncs_FuncReturnsFalse_RetryIsExecuted()
+  {
+    // Arrange
+    var successResult = false;
+    var executer = new AfterRenderExecuter();
+    executer.Add(() =>
+    {
+      // Flip the success result so that
+      // the 2nd time it'll return true
+      var result = successResult;
+      successResult = !successResult;
+      return result;
+    });
+
+    // Act
+    var (executedCount, failedCount) = executer.ExecuteFuncs();
+    var (retryExecutedCount, retryFailedCount) = executer.ExecuteFuncs();
+
+    // Assert
+    Assert.Equal(1, executedCount);
+    Assert.Equal(1, failedCount);
+
+    Assert.Equal(1, retryExecutedCount);
+    Assert.Equal(0, retryFailedCount);
+  }
+
 }
